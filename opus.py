9933eada5b16c34242fe0d435c806eb02034b8c0
Dietrich Epp <depp@zdome.net>
2018-08-14 02:43:11 -0400

    decode Opus headers

--- opus.py
+++ opus.py
@@ -1,4 +1,5 @@
 import argparse
+import io
 import struct
 
 import ogg
@@ -30,15 +31,15 @@ class OpusPacket:
         stereo = bool((toc >> 2) & 1)
         fr_code = toc & 3
         if config < 12:
-            mode = 'silk'
+            mode = "silk"
             rate = RATE_SILK[config >> 2]
             length = LENGTH_SILK[config & 3]
         elif config < 16:
-            mode = 'hybrid'
+            mode = "hybrid"
             rate = RATE_HYBRID[(config >> 1) & 1]
             length = LENGTH_HYBRID[config & 1]
         else:
-            mode = 'celt'
+            mode = "celt"
             rate = RATE_CELT[(config >> 2) & 3]
             length = LENGTH_CELT[config & 3]
         if fr_code == 0:
@@ -69,22 +70,150 @@ class OpusPacket:
             raise NotImplementedError("code 3 packets not supported")
         return class_(mode, rate, length, stereo, frames)
 
+class OpusFile:
+    def __init__(self, *, packets, vendor_string, comment_strings,
+                 channel_count, preskip, rate, gain):
+        self.packets = packets
+        self.vendor_string = vendor_string
+        self.comment_strings = comment_strings
+        self.channel_count = channel_count
+        self.preskip = preskip
+        self.rate = rate
+        self.gain = gain
+
+    @classmethod
+    def decode_stream(class_, stream):
+        if len(stream.pages) < 3:
+            raise OpusError("stream too short")
+
+        page = stream.pages[0]
+        if len(page.packets) != 1:
+            raise OpusError("invalid header page")
+        packet = page.packets[0]
+        if not packet.startswith(b"OpusHead"):
+            raise OpusError("missing header packet")
+        if len(packet) < 19:
+            raise OpusError("invalid header packet")
+        version, nchan, preskip, rate, gain, mapping = struct.unpack(
+            "<BBHIHB", packet[8:19])
+        if version != 1:
+            raise OpusError("unknown version {}".format(version))
+
+        page = stream.pages[1]
+        if len(page.packets) != 1:
+            raise OpusError("invalid comment page")
+        packet = page.packets[0]
+        if not packet.startswith(b"OpusTags"):
+            raise OpusError("missing comment packet")
+        i = 8
+        n = len(packet)
+        if 4 > n - i:
+            raise OpusError("invalid comment packet")
+        m, = struct.unpack("<I", packet[i:i+4])
+        i += 4
+        if m > n - i:
+            raise OpusError("invalid comment packet")
+        vstring = packet[i:i+m]
+        i += m
+        try:
+            vstring = vstring.decode("UTF-8")
+        except UnicodeDecodeError:
+            raise OpusError("invalid vendor string")
+        if 4 > n - i:
+            raise OpusError("invalid comment packet")
+        m, = struct.unpack("<I", packet[i:i+4])
+        i += 4
+        cstrings = []
+        for _ in range(m):
+            if 4 > n - i:
+                raise OpusError("invalid comment packet")
+            m, = struct.unpack("<i", packet[i:i+4])
+            i += 4
+            if m > n - i:
+                raise OpusError("invalid comment packet")
+            cstring = packet[i:i+m]
+            i += m
+            try:
+                cstring = cstring.decode("UTF-8")
+            except UnicodeDecodeError:
+                raise OpusError("invalid user string")
+            cstrings.append(cstring)
+
+        packets = []
+        for page in stream.pages[2:]:
+            for packet in page.packets:
+                packets.append(OpusPacket.decode(packet))
+
+        return class_(
+            packets=packets,
+            vendor_string=vstring,
+            comment_strings=cstrings,
+            channel_count=nchan,
+            preskip=preskip,
+            rate=rate,
+            gain=gain,
+        )
+
+    @classmethod
+    def decode_ogg(class_, data):
+        for stream in ogg.parse_ogg(data):
+            if stream.pages[0].packets[0].startswith(b"OpusHead"):
+                return class_.decode_stream(stream)
+        raise OpusError("no opus stream")
+
+    def dump(self):
+        print("Header: preskip={0.preskip} rate={0.rate} gain={0.gain}"
+              .format(self))
+        print("Comment:")
+        print("  Vendor: {!r}".format(self.vendor_string))
+        for cstring in self.comment_strings:
+            print("  Comment: {!r}".format(cstring))
+        print("Total Packet size:", sum(len(frame) for packet in self.packets
+                                        for frame in packet.frames))
+        for packet in self.packets:
+            print("  Packet mode={0.mode} rate={0.rate} "
+                  "length={0.length} stereo={0.stereo} frames={1}"
+                  .format(packet, ", ".join(str(len(f))
+                                                for f in packet.frames)))
+
+    def to_ogg(self):
+        pages = []
+        fp = io.BytesIO()
+
+        fp.write(b"OpusHead")
+        fp.write(struct.pack(
+             "<BBHIHB", 1,
+             self.channel_count, self.preskip, self.rate, self.gain, 0))
+        pages.append(ogg.OggPage(0, [fp.getvalue()]))
+
+        fp.seek(0)
+        fp.truncate()
+        fp.write(b"OpusTags")
+        cstring = self.vendor_string.encode("UTF-8")
+        fp.write(struct.pack("<I", len(cstring)))
+        fp.write(cstring)
+        fp.write(struct.pack("<I", len(self.comment_strings)))
+        for cstring in self.comment_strings:
+            cstring = cstring.encode("UTF-8")
+            fp.write(struct.pack("<I", len(cstring)))
+            fp.write(cstring)
+        pages.append(ogg.OggPage(0, [fp.getvalue()]))
+
+    def to_bytes(self):
+        return self.to_ogg().to_bytes()
+
 def main():
     p = argparse.ArgumentParser(allow_abbrev=False)
-    p.add_argument("input", help="input Opus files", nargs="+")
+    p.add_argument("-input", help="input Opus file", required=True)
+    p.add_argument("-output", help="output Opus file", required=True)
     args = p.parse_args()
 
-    for fname in args.input:
-        with open(fname, 'rb') as fp:
-            data = fp.read()
-        for stream in ogg.parse_ogg(data):
-            for page in stream.pages[2:]:
-                for packet in page.packets:
-                    packet = OpusPacket.decode(packet)
-                    print("Packet mode={0.mode} rate={0.rate} "
-                          "length={0.length} stereo={0.stereo} frames={1}"
-                          .format(packet, ", ".join(str(len(f))
-                                                    for f in packet.frames)))
+    with open(args.input, "rb") as fp:
+        data = fp.read()
+    opus = OpusFile.decode_ogg(data)
+    opus.dump()
+    with open(args.output, "wb") as fp:
+        fp.write(opus.to_bytes())
 
 if __name__ == "__main__":
     main()
