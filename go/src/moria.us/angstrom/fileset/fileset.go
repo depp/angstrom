a8174c1b5fab6a0c9e86b06cf24720acae2c2ff0
Dietrich Epp <depp@zdome.net>
2018-09-06 02:02:33 -0400

    fix more panics in fileset
    Almost as if I should have written unit tests.

--- go/src/moria.us/angstrom/fileset/fileset.go
+++ go/src/moria.us/angstrom/fileset/fileset.go
@@ -128,10 +128,11 @@ func (s *Set) Expire(stamp time.Time) {
 		return
 	}
 	copy(s.expiring, s.expiring[n:])
-	for i := range s.expiring[:len(s.expiring)-n] {
-		s.expiring[i] = nil
+	j := len(s.expiring) - n
+	for i := range s.expiring[j:] {
+		s.expiring[j+i] = nil
 	}
-	s.expiring = s.expiring[:len(s.expiring)-n]
+	s.expiring = s.expiring[:j]
 }
 
 // GetVersion returns a specific version of a file.
