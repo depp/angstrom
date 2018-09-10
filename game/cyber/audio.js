6f3e2924b5bd14a4f4caf284b39546d7ea95daea
Dietrich Epp <depp@zdome.net>
2018-09-10 18:48:28 -0400

    make startup sound almost silent
    size=6315

--- game/cyber/audio.js
+++ game/cyber/audio.js
@@ -1,15 +1,20 @@
 let audioCtx;
 
+// Start the audio system, if it is not already started. This must be called
+// from an event handler in order to get audio permissions across most browsers
+// (especially Safari, but Chrome 70 will also need this).
 export function startAudio() {
   if (audioCtx) {
     return;
   }
   audioCtx = new AudioContext();
 
+  const t0 = audioCtx.currentTime;
+  const g = audioCtx.createGain();
+  g.gain.setValueAtTime(0.01, t0);
+  g.connect(audioCtx.destination);
   const osc = audioCtx.createOscillator();
-  osc.type = 'square';
-  osc.frequency.setValueAtTime(1000, audioCtx.currentTime);
-  osc.connect(audioCtx.destination);
+  osc.connect(g);
   osc.start();
-  osc.stop(audioCtx.currentTime + 1);
+  osc.stop(audioCtx.currentTime + 0.05);
 }
