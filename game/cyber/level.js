5dceacfb940dbccd4dc275b426775021f5ae6fb3
Dietrich Epp <depp@zdome.net>
2018-09-12 03:17:17 -0400

    generate tile edges
    size=7608

--- game/cyber/level.js
+++ game/cyber/level.js
@@ -16,11 +16,10 @@ class Level {
     gl.bindBuffer(gl.ARRAY_BUFFER, this.meshBuffer);
     if (this.meshDirty) {
       const mesh = this.meshBuildFunc();
-      console.log(`SZ: ${mesh.length}`);
       this.vertexCount = mesh.length / 5;
       gl.bufferData(gl.ARRAY_BUFFER, mesh, gl.STATIC_DRAW);
       this.meshDirty = false;
-      console.log(`Generated ${this.vertexCount} vertices`);
+      // console.log(`Generated ${this.vertexCount} vertices`);
     }
     return this.vertexCount;
   }
@@ -39,15 +38,13 @@ export const levels = [];
     if (tiles[pos]) {
       return;
     }
-    tiles[pos] = 1;
+    tiles[pos] = -Math.random();
   }
 
-  /*
   // Get the value of a single tile.
   function getTile(...pos) {
     return tiles[pos];
   }
-  */
 
   // Make a rectangle floor.
   function floorRect(xo, yo, xs, ys) {
@@ -73,6 +70,8 @@ export const levels = [];
     levels[levelNumber] = level;
   }
 
+  const neighbors = [-1, 0, 1, 0, -1];
+
   function createMesh() {
     let arr = new Float32Array(256);
     let pos = 0;
@@ -80,7 +79,7 @@ export const levels = [];
     let y;
     // Emit a single mesh triangle.
     function emit(uAxis, vAxis, ...verts) {
-      if (5 * 3 > arr.length - pos) {
+      if (5 * verts.length > arr.length - pos) {
         const narr = new Float32Array(arr.length * 2);
         narr.set(arr);
         arr = narr;
@@ -98,10 +97,10 @@ export const levels = [];
     /* eslint-disable guard-for-in */
     for (const key in tiles) {
       /* eslint-enable guard-for-in */
+      const z = tiles[key];
       [x, y] = key.split(',');
       x = +x;
       y = +y;
-      const z = -Math.random();
       for (let side = 0; side < 4; side++) {
         const v0 = ((side + 0) >> 1) & 1; // y0
         const v1 = ((side + 1) >> 1) & 1; // x0, y1
@@ -112,6 +111,21 @@ export const levels = [];
           [v2, v1, z],
           [vc, vc, z + 0.2],
         );
+        const nz = getTile(
+          x + neighbors[side + 1],
+          y + neighbors[side],
+        );
+        if (nz != null && nz < z) {
+          emit(
+            side & 1, 2,
+            [v1, v0, z],
+            [v1, v0, nz],
+            [v2, v1, z],
+            [v2, v1, z],
+            [v1, v0, nz],
+            [v2, v1, nz],
+          );
+        }
       }
     }
     return arr.slice(0, pos);
