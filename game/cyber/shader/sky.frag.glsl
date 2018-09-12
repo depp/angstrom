c51f44b838428c5801450023c83f3fa694141f09
Dietrich Epp <depp@zdome.net>
2018-09-12 19:35:35 -0400

    optimize the sky shader a little
    size=10023

--- game/cyber/shader/sky.frag.glsl
+++ game/cyber/shader/sky.frag.glsl
@@ -7,9 +7,15 @@ uniform vec4 LightColor[8];
 const mat3 NormalSpace = mat3(1.0);
 const vec3 Pos = vec3(0.0);
 
-#include "lighting.glsl"
-
 void main() {
-    gl_FragColor = vec4(light(Direction), 1.0);
+    vec3 lighting, light, normal = normalize(Direction);
+    for (int i = 0; i < 8; i++) {
+        light = normalize(LightPos[i].xyz - LightPos[i].w * Pos);
+        float d = mix(dot(normal, light), 1.0, LightColor[i].a);
+        if (d > 0.0) {
+            lighting += d * LightColor[i].rgb;
+        }
+    }
+    gl_FragColor = vec4(1.0 - 1.0 / (lighting + 1.0), 1.0);
     // gl_FragColor = vec4(Direction * 0.5 + 0.5, 1.0);
 }
